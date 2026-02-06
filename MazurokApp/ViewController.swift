f8d711549f074ef9694b654b1790a4ff364460da
Andrii Mazurok <andrew_maz@ukr.net>
2026-02-06 22:25:11 +0200

    Initial Commit

---
+++ MazurokApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MazurokApp
+//
+//  Created by Andrii Mazurok on 06.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
